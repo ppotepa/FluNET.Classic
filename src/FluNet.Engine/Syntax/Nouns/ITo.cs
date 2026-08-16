00b837b758489dbe055ad5e072a6a6ddbcba946f
Paweł Potępa <potepapawel@gmail.com>
2026-08-16 20:00:14 +0200

    Unify TO with language role model

--- src/FluNet.Engine/Syntax/Nouns/ITo.cs
+++ src/FluNet.Engine/Syntax/Nouns/ITo.cs
@@ -1,18 +1,10 @@
 using FluNET.Keywords;
+using FluNET.Language;
 using FluNET.Syntax.Core;
 
-namespace FluNET.Syntax.Nouns
+namespace FluNET.Syntax.Nouns;
+
+public interface ITo<out TValue> : INoun, IKeyword, IRole<TValue>
 {
-    /// <summary>
-    /// Represents a destination preposition - where something goes to.
-    /// Example: In "SAVE data TO [output.txt]", [output.txt] implements ITo&lt;FileInfo&gt;.
-    /// </summary>
-    /// <typeparam name="TTo">The type of the destination</typeparam>
-    public interface ITo<out TTo> : INoun, IKeyword
-    {
-        /// <summary>
-        /// The destination where data is sent or saved.
-        /// </summary>
-        TTo To { get; }
-    }
+    TValue To { get; }
 }
