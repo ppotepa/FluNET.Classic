e3cf7fb8959785c8da297a75b6e51edd496a8d99
Paweł Potępa <potepapawel@gmail.com>
2026-08-16 20:00:03 +0200

    Unify FROM with language role model

--- src/FluNet.Engine/Syntax/Nouns/IFrom.cs
+++ src/FluNet.Engine/Syntax/Nouns/IFrom.cs
@@ -1,18 +1,10 @@
 using FluNET.Keywords;
+using FluNET.Language;
 using FluNET.Syntax.Core;
 
-namespace FluNET.Syntax.Nouns
+namespace FluNET.Syntax.Nouns;
+
+public interface IFrom<out TValue> : INoun, IKeyword, IRole<TValue>
 {
-    /// <summary>
-    /// Represents a source or origin preposition - where something comes from.
-    /// Example: In "GET data FROM [file.txt]", [file.txt] implements IFrom&lt;FileInfo&gt;.
-    /// </summary>
-    /// <typeparam name="TWhat">The type of the source/origin</typeparam>
-    public interface IFrom<out TWhat> : INoun, IKeyword
-    {
-        /// <summary>
-        /// The source or origin from which data is retrieved.
-        /// </summary>
-        TWhat From { get; }
-    }
+    TValue From { get; }
 }
