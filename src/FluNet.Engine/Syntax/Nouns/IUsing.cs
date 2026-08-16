6f0100810356e1fe04637ba215e96ed1ef119209
Paweł Potępa <potepapawel@gmail.com>
2026-08-16 20:00:22 +0200

    Unify USING with language role model

--- src/FluNet.Engine/Syntax/Nouns/IUsing.cs
+++ src/FluNet.Engine/Syntax/Nouns/IUsing.cs
@@ -1,18 +1,10 @@
 using FluNET.Keywords;
+using FluNET.Language;
 using FluNET.Syntax.Core;
 
-namespace FluNET.Syntax.Nouns
+namespace FluNET.Syntax.Nouns;
+
+public interface IUsing<out TValue> : INoun, IKeyword, IRole<TValue>
 {
-    /// <summary>
-    /// Represents an instrument preposition - the tool, method, or means by which an action is performed.
-    /// Example: In "ENCRYPT data USING [AES256]", [AES256] implements IUsing&lt;Algorithm&gt;.
-    /// </summary>
-    /// <typeparam name="TUsing">The type of the instrument or method</typeparam>
-    public interface IUsing<out TUsing> : INoun, IKeyword
-    {
-        /// <summary>
-        /// The instrument, tool, or method used to perform the action.
-        /// </summary>
-        TUsing Using { get; }
-    }
+    TValue Using { get; }
 }
