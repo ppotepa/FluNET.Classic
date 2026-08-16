a49673d67871353cb976417dfe21cd545b8d6c68
Paweł Potępa <potepapawel@gmail.com>
2026-08-16 20:00:30 +0200

    Unify WITH with language role model

--- src/FluNet.Engine/Syntax/Nouns/IWith.cs
+++ src/FluNet.Engine/Syntax/Nouns/IWith.cs
@@ -1,18 +1,10 @@
 using FluNET.Keywords;
+using FluNET.Language;
 using FluNET.Syntax.Core;
 
-namespace FluNET.Syntax.Nouns
+namespace FluNET.Syntax.Nouns;
+
+public interface IWith<out TValue> : INoun, IKeyword, IRole<TValue>
 {
-    /// <summary>
-    /// Represents an accompaniment preposition - what accompanies or is used with an action.
-    /// Example: In "CONNECT TO server WITH [credentials]", [credentials] implements IWith&lt;AuthToken&gt;.
-    /// </summary>
-    /// <typeparam name="TWith">The type of the accompanying element</typeparam>
-    public interface IWith<out TWith> : INoun, IKeyword
-    {
-        /// <summary>
-        /// The accompanying element used with the action.
-        /// </summary>
-        TWith With { get; }
-    }
+    TValue With { get; }
 }
