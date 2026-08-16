65b23ea6dcdb07986f39f1c85feee00c1b673a8e
Paweł Potępa <potepapawel@gmail.com>
2026-08-16 20:00:42 +0200

    Unify THEN with language role model

--- src/FluNet.Engine/Syntax/Nouns/IThen.cs
+++ src/FluNet.Engine/Syntax/Nouns/IThen.cs
@@ -1,20 +1,10 @@
 using FluNET.Keywords;
+using FluNET.Language;
 using FluNET.Syntax.Core;
 
-namespace FluNET.Syntax.Nouns
+namespace FluNET.Syntax.Nouns;
+
+public interface IThen<out TValue> : INoun, IKeyword, IRole<TValue>
 {
-    /// <summary>
-    /// Represents a continuation of a sentence that operates on the same data.
-    /// Allows chaining multiple operations on the same data source.
-    /// Example: GET [data] FROM source THEN SAVE TO file.
-    /// </summary>
-    /// <typeparam name="TData">The type of data being processed through the chain</typeparam>
-    public interface IThen<out TData> : INoun, IKeyword
-    {
-        /// <summary>
-        /// Gets the data that is being passed to the next operation in the chain.
-        /// This is the result from the previous verb's execution.
-        /// </summary>
-        TData Data { get; }
-    }
+    TValue Then { get; }
 }
