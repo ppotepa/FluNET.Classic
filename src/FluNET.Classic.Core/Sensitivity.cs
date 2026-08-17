2e64c37371044192b323001660068b4a4e027992
Paweł Potępa <potepapawel@gmail.com>
2026-08-18 00:18:14 +0200

    Batch 15A: add semantic sensitivity metadata

---
+++ src/FluNET.Classic.Core/Sensitivity.cs
@@ -0,0 +1,15 @@
+namespace FluNET.Classic.Core;
+
+public static class SensitiveValueMetadata
+{
+    public static bool IsSensitiveType(Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+        Type effective = Nullable.GetUnderlyingType(type) ?? type;
+        if (typeof(ISensitiveValue).IsAssignableFrom(effective)) return true;
+        Type? element = ClrTypeShape.GetElementType(effective);
+        return element is not null && element != effective && IsSensitiveType(element);
+    }
+
+    public static bool IsSensitiveValue(object? value) => value is ISensitiveValue;
+}
